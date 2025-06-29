6ba7e39670cb9dbc87785f6907f9ea2ead02af81
Anatolii Mychta <anatolii_mychta@epam.com>
2025-06-29 23:39:16 +0300

    My Anatolii's Initial commit: Personal Docusaurus site about food and recipes

---
+++ sidebars.js
@@ -0,0 +1,16 @@
+const sidebars = {
+  tutorialSidebar: [
+    {
+      type: 'category',
+      label: 'Eating – Anatolii 🍽️',
+      link: { type: 'doc', id: 'eating/index' },
+      items: [
+        'eating/buy',
+        'eating/receipts',
+        'eating/supplements',
+      ],
+    },
+  ],
+};
+
+export default sidebars;
